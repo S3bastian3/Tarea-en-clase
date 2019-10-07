1ac626376181860a3d6d0c277d4e87c734df6142
S3bastian3 <55717651+S3bastian3@users.noreply.github.com>
2019-10-07 16:45:40 -0500

    Add files via upload

---
+++ EJERCICIO 2 Sebastian sanchez.c
@@ -0,0 +1,8 @@
+#include<stdio.h>  //aqui se incluye la libreria 
+void main () //se especifica que no devuelve ningun valor
+{//se abre llaves para realizar el programa
+	//Aqui se imprimen los asteriscos acomodanlodos para que formen la palabra "HOLA MUNDO"
+	printf ("*  * **** *    ****    *   * *  * *  * ***  ****\n");
+	printf ("**** *  * *    ****    * * * *  * * ** *  * *  *\n");
+	printf ("*  * **** **** *  *    *   * **** *  * ***  ****\n");
+}//se cierra las llaves 
